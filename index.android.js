b4960b1f86cbf32de08b6f5eae24fac4ca5a3284
frank <Frank Dattalo>
2016-11-19 19:33:09 -0500

    test tube

--- index.android.js
+++ index.android.js
@@ -18,88 +18,19 @@ import {
 import Button from 'react-native-button'
 import { RadioButtons } from 'react-native-radio-buttons'
 
-var SCREEN_WIDTH = require('Dimensions').get('window').width;
-var BaseConfig = Navigator.SceneConfigs.FloatFromRight;
-var backConfig = Navigator.SceneConfigs.FloatFromLeft;
 
-var CustomSceneConfig = Object.assign({}, BaseConfig, {
-  // A very tighly wound spring will make this transition fast
-  springTension: 100,
-  springFriction: 1,
-});
-
-var CustomBackSceneConfig = Object.assign({}, backConfig, {
-  springTension: -100,
-  springFriction: 1,
-});
-
-var bars = ["Midway", "Toos", "Big Bar", "Lucky's"];
 
 export default class BarCrawlApp extends Component {
 
-  // constructor(props) {
-  //   super(props);
-  //   const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-  //        this.state = {
-  //         dataSource: ds.cloneWithRows([
-  //           "1Bar", "2Bar", "Red Fish", "Blue Fish"
-  //         ])
-  //       };
-  //  }
+
 
   render() {
     return (
       <View style={styles.main}>
-        <ToolbarAndroid
-           
-           onIconClicked={() => this.drawer.openDrawer()}
-           style={styles.toolbar}
-           actions={[{title:'hey guys'}]} />
-        <View style={styles.container}>
-          <Text style={styles.welcome}>Bars Neer You</Text>
-          <RadioButtons
-            options={bars}
-            onSelection={ this.setSelectedOption}
-            selectedOption={this.selectedOption }
-            renderOption={ this.renderOption }
-            renderContainer={ this.renderContainer }/>
-        </View>
+        <Text>Hello World</Text>
       </View>
     );
   }
-
-  setSelectedOption(selectedOption){
-    this.setState({
-      selectedOption
-    });
-  }
-
- renderOption(option, selected, onSelect, index){
-   const style = selected ? { fontWeight: 'bold'} : {};
-   return (
-     <TouchableWithoutFeedback onPress={onSelect} key={index}>
-      <View>
-       <Text>{option}</Text>
-      </View>
-     </TouchableWithoutFeedback>
-   )
-
-}
-renderContainer(optionNodes){
-  return <View>{optionNodes}</View>;
-}
-
-onValueChange(key: string, value: string) {
-  const newState = {};
-  newState[key] = value;
-  this.setState(newState);
-}
-
-
-  _handleClick(){
-
-  }
-
 
 }
 
